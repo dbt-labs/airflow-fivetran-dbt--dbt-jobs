c7cce62e8939d32956f5b4fab0d8c155c3e60156
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-01-20 00:01:16 +0000

    adds pokedex source

---
+++ models/staging/pokemon/stg_pokedex.sql
@@ -0,0 +1,11 @@
+with pokedex as ( select * from {{source('pokemon', 'pokedex') }} ), 
+
+final as (
+    select nat as "pokemon_id",
+           hatch as "hatch_number",
+           catch as "catch_number",
+           hp as "heart_power" 
+    from pokedex
+)
+
+select * from final
